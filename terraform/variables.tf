4137af1dfd44ca31e664ec4e554edf2a9cc47dae
Michael Thamm <mike.thamm@canonical.com>
2026-03-20 14:30:26 -0400

    feat: channel validation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,6 +8,13 @@ variable "app_name" {
 variable "channel" {
   description = "Channel that the charm is deployed from"
   type        = string
+  default     = "dev/edge"
+
+  validation {
+    # the TF Juju provider correctly identifies invalid risks; no need to validate it
+    condition     = startswith(var.channel, "dev/")
+    error_message = "The track of the channel must be 'dev/'. e.g. 'dev/edge'."
+  }
 }
 
 variable "config" {
